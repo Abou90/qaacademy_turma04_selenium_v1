25ab1cfaa999f7de36204e8afdcddf0e4ddb1b9b
Edmon <edmon.abou@gmail.com>
2022-05-14 12:38:46 -0300

    Atualização até select country

--- src/test/java/ExerciciosSelenium.java
+++ src/test/java/ExerciciosSelenium.java
@@ -10,7 +10,7 @@ public class ExerciciosSelenium {
 
     @Test
     public void testeExercicosSelenium() {
-        driver.get("file:///Users/mac/Documents/automation.html");
+        driver.get("file:///D:/Documentos/QA%20-%20AUTOMATIZA%C3%87%C3%83O/Paginas%20para%20testes/Register.html");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[1]/div[1]/input[1]"))
                 .sendKeys("Flavio");
         driver.findElement(By.xpath("//body[@class='ng-scope']/section[@id='section']/div[@class='container center']/div[contains(@class,'row')]/div[@class='col-sm-6 col-md-6 col-xs-12']/form[@id='basicBootstrapForm']/div[@class='form-group']/div[2]/input[1]"))
@@ -18,5 +18,15 @@ public class ExerciciosSelenium {
         driver.findElement(By.xpath("//textarea[@ng-model='Adress']")).sendKeys("AV Paulista, 509");
         driver.findElement(By.xpath("//input[@type='email']")).sendKeys("teste@teste.com.br");
         driver.findElement(By.xpath(" //input[@type='tel']")).sendKeys("119773512");
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[5]/div/label[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"checkbox2\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"msdd\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"basicBootstrapForm\"]/div[7]/div/multi-select/div[2]/ul/li[8]/a")).click();
+        driver.findElement(By.xpath("/html/body")).click();
+        driver.findElement(By.xpath("//*[@id=\"Skills\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"Skills\"]/option[39]")).click();
+        driver.findElement(By.xpath("//*[@id=\"countries\"]")).click();
+        driver.findElement(By.xpath("//*[@id=\"select2-country-container\"]")).click();
+
     }
 }
